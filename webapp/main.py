3cecf67089f52cbde4f8363df1d131e69ad27953
Brian Boerst <brian@boerst.com>
2024-06-02 14:44:36 -0500

    Better CORS

--- webapp/main.py
+++ webapp/main.py
@@ -16,8 +16,8 @@ mongodb_password = os.environ.get('MONGODB_PASSWORD')
 mongodb_hosts = os.environ.get('MONGODB_HOSTS')
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:8000"}})
-socketio = SocketIO(app, cors_allowed_origins="http://127.0.0.1:8000")
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:8000", "https://poolwork.live"]}})
+socketio = SocketIO(app, cors_allowed_origins=["http://127.0.0.1:8000", "https://poolwork.live"])
 
 # MongoDB connection with authentication and connection pooling
 client = MongoClient(f"mongodb://{mongodb_username}:{mongodb_password}@{mongodb_hosts}", maxPoolSize=50)

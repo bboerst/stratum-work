3ee37b42e5b0d546095ec9c8b86dce6c5f6037e7
boerst <brian@boerst.com>
2025-02-16 11:10:03 -0600

    Default dark mode 🌙 (#34)

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="dark" storageKey="theme-default-dark">
           {children}
         </ThemeProvider>
       </body>

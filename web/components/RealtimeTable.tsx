d0833a153c956ac0d9b547f6d024a05d8bc2b194
Brian Boerst <brian@boerst.com>
2025-03-09 17:14:08 -0500

    Removing loading verbiage

--- web/components/RealtimeTable.tsx
+++ web/components/RealtimeTable.tsx
@@ -169,7 +169,6 @@ export default function RealtimeTable({
         <div className="fixed inset-x-0 top-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-lg text-center">
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-500 mx-auto mb-2"></div>
-            <p className="text-lg font-semibold">Loading History...</p>
           </div>
         </div>
       )}

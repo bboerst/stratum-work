929d1fa4fcad63597564f5000bef7100be2dd0c0
boerst <brian@boerst.com>
2026-03-09 09:41:54 -0500

    Cleanup

--- lib/BlocksContext.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React, { createContext, useContext, useState, useRef, ReactNode, MutableRefObject } from 'react';
-import { Block } from '@/types/blockTypes';
-
-interface BlocksContextType {
-  blocks: Block[];
-}
